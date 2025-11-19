46b6a6391a38acdd83ee931cf79685791176708a
Rafael <tradeautomacao.dev1@gmail.com>
2025-11-18 23:16:47 -0300

    Adicionado ao clicar na logo do cabeçalho navegar para a rota principal

--- projeto/src/components/cabecalho.jsx
+++ projeto/src/components/cabecalho.jsx
@@ -5,7 +5,11 @@ import Logo from '../assets/logo.svg?react'
 export default function Cabecalho() {
   return (
     <header className="flex justify-between items-center w-full p-4 lg:px-20 bg-primary text-white">
-      <Logo />
+
+      <Link to="/">
+        <Logo className="cursor-pointer"/>
+      </Link>
+
       <div className="flex gap-2">
         <Link to='/login'>login</Link>
         <Link to='/'>Adm</Link>
